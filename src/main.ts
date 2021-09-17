662a5c69738c3887605feeca54514f98815dc8ec
黄鸿健 <2443005004@qq.com>
2021-09-17 11:14:16 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+
+Vue.prototype.$store = store
 
 Vue.config.productionTip = false
 
 new App().$mount()
+
+
---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);//vue的插件机制
+
+//Vuex.Store 构造器选项
+const store = new Vuex.Store({
+    state:{
+    },
+    getters: {
+
+    },
+    mutations: {},
+    actions: {},
+    
+})
+export default store
